d2264e87f42415d213b72c7329825972e4bcd3a4
LR.Snow <lrsnowx@gmail.com>
2024-03-25 17:11:06 +0800

    update tests.m, fix some test problom

--- Frameworks/Tests.m
+++ Frameworks/Tests.m
@@ -1,8 +1,5 @@
 #import <XCTest/XCTest.h>
-
-@import GCDWebServers;
-
-#pragma clang diagnostic ignored "-Weverything"  // Prevent "messaging to unqualified id" warnings
+#import <GCDWebServers/GCDWebServers.h>
 
 @interface Tests : XCTestCase
 @end
@@ -10,35 +7,35 @@
 @implementation Tests
 
 - (void)testWebServer {
-  GCDWebServer* server = [[GCDWebServer alloc] init];
-  XCTAssertNotNil(server);
+    GCDWebServer* server = [[GCDWebServer alloc] init];
+    XCTAssertNotNil(server);
 }
 
 - (void)testDAVServer {
-  GCDWebDAVServer* server = [[GCDWebDAVServer alloc] init];
-  XCTAssertNotNil(server);
+    GCDWebDAVServer* server = [[GCDWebDAVServer alloc] init];
+    XCTAssertNotNil(server);
 }
 
 - (void)testWebUploader {
-  GCDWebUploader* server = [[GCDWebUploader alloc] init];
-  XCTAssertNotNil(server);
+    GCDWebUploader* server = [[GCDWebUploader alloc] init];
+    XCTAssertNotNil(server);
 }
 
 - (void)testPaths {
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@""), @"");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@"/foo/"), @"/foo");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@"foo/bar"), @"foo/bar");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@"foo//bar"), @"foo/bar");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@"foo/bar//"), @"foo/bar");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@"foo/./bar"), @"foo/bar");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@"foo/bar/."), @"foo/bar");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@"foo/../bar"), @"bar");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@"/foo/../bar"), @"/bar");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@"/foo/.."), @"/");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@"/.."), @"/");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@"."), @"");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@".."), @"");
-  XCTAssertEqualObjects(GCDWebServerNormalizePath(@"../.."), @"");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@""), @"");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@"/foo/"), @"/foo");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@"foo/bar"), @"foo/bar");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@"foo//bar"), @"foo/bar");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@"foo/bar//"), @"foo/bar");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@"foo/./bar"), @"foo/bar");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@"foo/bar/."), @"foo/bar");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@"foo/../bar"), @"bar");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@"/foo/../bar"), @"/bar");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@"/foo/.."), @"/");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@"/.."), @"/");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@"."), @"");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@".."), @"");
+    XCTAssertEqualObjects(GCDWebServerNormalizePath(@"../.."), @"");
 }
 
 @end
